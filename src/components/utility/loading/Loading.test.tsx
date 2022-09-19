77f26b5cd24c7213887485b90b6c8ff0d2a35eec
theRealGG <hisham30g@gmail.com>
2022-09-19 08:05:55 +0200

    Added translation mocking to Loading component

--- src/components/utility/loading/Loading.test.tsx
+++ src/components/utility/loading/Loading.test.tsx
@@ -1,6 +1,22 @@
 import { render } from '@testing-library/react';
 import Loading from './Loading';
 
+jest.mock('react-i18next', () => ({
+  __esModule: true,
+  useTranslation: () => {
+    return {
+      t: () => 'Loading',
+      i18n: {
+        changeLanguage: () => new Promise(() => {})
+      }
+    };
+  },
+  initReactI18next: {
+    type: '3rdParty',
+    init: jest.fn()
+  }
+}));
+
 describe('Test Loading (splash screen) Component', () => {
   it('renders', () => {
     const { baseElement } = render(<Loading />);
