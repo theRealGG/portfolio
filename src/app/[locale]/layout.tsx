a3c8dc7374ce61ff17e46e835b72f0ed655ae16c
Hisham Görner <hisham30g@gmail.com>
2024-02-22 14:56:50 +0100

    modified localized layout

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -4,7 +4,9 @@ import { PropsWithChildren } from 'react';
 import { locales } from '@/config';
 import font from '@/style/font';
 import pick from 'lodash/pick';
+import { Metadata } from 'next';
 import { NextIntlClientProvider, useMessages } from 'next-intl';
+import Footer from '@/components/footer';
 
 interface LocaleLayoutProps {
   params: { locale: string };
@@ -14,11 +16,40 @@ export function generateStaticParams() {
   return locales.map((locale) => ({ locale }));
 }
 
-export async function generateMetadata({ params: { locale } }: LocaleLayoutProps) {
-  const t = await getTranslations({ locale, namespace: 'layout' });
-
+export async function generateMetadata({ params: { locale } }: LocaleLayoutProps): Promise<Metadata> {
+  const t = await getTranslations({ locale, namespace: 'metadata' });
+  const url = new URL('https://acme.com');
   return {
-    title: t('title'),
+    title: {
+      default: t('title', { name: 'Hisham Görner' }),
+      template: 'Hisham Görner | %s',
+    },
+    description: t('description'),
+    referrer: 'origin-when-cross-origin',
+    keywords: ['Hisham Görner', 'Frontend', 'Backend', 'Next.js', 'Angular', 'Spring', 'Quarkus', 'Docker'],
+    creator: 'Hisham Görner',
+    publisher: 'Hisham Görner',
+    formatDetection: {
+      email: true,
+      address: false,
+      telephone: false,
+    },
+    metadataBase: url,
+    alternates: {
+      canonical: '/',
+      languages: {
+        'en-US': '/en',
+        'de-DE': '/de',
+      },
+    },
+    openGraph: {
+      title: t('title', { name: 'Hisham Görner' }),
+      type: 'website',
+      url,
+      images: '/hisham.png',
+      emails: 'mailto:hisham30g@gmail.com',
+      countryName: 'Germany',
+    },
   };
 }
 
@@ -30,7 +61,8 @@ export default function LocaleLayout({ children, params: { locale } }: PropsWith
     <html className="h-full" lang={locale}>
       <body className={clsx(font.variable, 'flex h-full flex-col')}>
         <NextIntlClientProvider locale={locale} messages={pick(messages, 'error')}>
-          {children}
+          <main className="flex-1">{children}</main>
+          <Footer />
         </NextIntlClientProvider>
       </body>
     </html>
