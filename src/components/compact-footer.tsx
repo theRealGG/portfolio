ba17e740a18c444c7269c196f6def5a85523bb3b
Hisham Görner <hisham30g@gmail.com>
2024-02-22 14:57:26 +0100

    modified components

--- src/components/compact-footer.tsx
+++ src/components/compact-footer.tsx
@@ -4,7 +4,7 @@ export default function Footer({ links }: { links: SocialLinks }) {
   return (
     <footer className="border-t border-gray-100 py-6 sm:py-10">
       <div className="mx-auto flex max-w-7xl flex-col items-center justify-center gap-8 px-6 sm:flex-row lg:px-8">
-        <p className="text-sm leading-7 text-gray-400">&copy; Your Company, Inc. All rights reserved.</p>
+        <p className="text-sm leading-7 text-gray-400">&copy; Hisham Görner</p>
         <div className="hidden sm:block sm:h-7 sm:w-px sm:flex-none sm:bg-gray-200" />
         <div className="flex gap-x-4">
           {links.map((item) => (
---
+++ src/components/footer.tsx
@@ -0,0 +1,40 @@
+import { routes, socialLinks } from '@/shared/utility';
+import { Link } from '@/navigation';
+import { useTranslations } from 'next-intl';
+import { Text } from '@/components/ui';
+
+export default function Footer() {
+  const t = useTranslations();
+  const localizedRoutes = routes.map((r) => ({ ...r, name: t(r.name as any), description: t(r.description as any) }));
+  const localizedSocial = socialLinks.map((r) => ({
+    ...r,
+    name: t(r.name as any),
+    description: t(r.description as any),
+  }));
+  return (
+    <footer className="bg-white">
+      <div className="mx-auto max-w-7xl overflow-hidden px-6 py-20 sm:py-24 lg:px-8">
+        <nav className="-mb-6 flex items-center justify-center gap-x-4 sm:space-x-12" aria-label="Footer">
+          {localizedRoutes.map((item) => (
+            <div key={item.key} className="pb-6">
+              <Text as={Link} href={item.href} intent="note" text="primary">
+                <item.icon className="h-6 w-6 text-charcoal hover:text-zomp sm:hidden" />
+                <span className="hidden hover:text-zomp sm:block"> {item.name} </span>
+              </Text>
+            </div>
+          ))}
+        </nav>
+        <div className="mt-10 flex justify-center space-x-10">
+          {localizedSocial.map((item) => (
+            <Text as="a" key={item.key} href={item.href} intent="text" text="secondary">
+              <span className="sr-only">{item.name}</span>
+              <item.icon className="h-6 w-6" aria-hidden="true" />
+            </Text>
+          ))}
+        </div>
+
+        <p className="mt-10 text-center text-xs leading-5 text-gray-500">&copy; 2024 Hisham Görner</p>
+      </div>
+    </footer>
+  );
+}
