8aed1a08b472dde069d5bfa616bdbc77f9a9117e
Hisham Görner <hisham30g@gmail.com>
2024-10-13 14:37:18 +0200

    added shadcn

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,4 +1,3 @@
-import clsx from 'clsx';
 import { getMessages, unstable_setRequestLocale } from 'next-intl/server';
 import { PropsWithChildren } from 'react';
 import { Provider } from '@/components';
@@ -20,13 +19,7 @@ export default async function LocaleLayout({
 
   return (
     <html className="h-full" lang={locale}>
-      <body
-        className={clsx(
-          poppins.className,
-          'bg-white dark:bg-rich-black',
-          'text-davys-gray dark:text-french-gray',
-        )}
-      >
+      <body className={poppins.className}>
         <Provider messages={messages}>{children}</Provider>
       </body>
     </html>
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import { PropsWithChildren } from 'react';
-import '@/style/style.css';
+import './globals.css';
 
 export default function RootLayout({ children }: PropsWithChildren) {
   return children;
